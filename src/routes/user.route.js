c73b5ccec23fc9320fc35888c881db6d0eb65a0b
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2024-03-24 21:38:51 +0530

    define users Routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { changeCurrentPasssword, logOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPasssword,
+        getCurrentUser,
+        getUserChannelProfile,
+        getWatchHistory,
+        logOutUser,
+        loginUser, 
+        refreshAccessToken, 
+        registerUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImge
+     } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -18,8 +29,18 @@ router.route("/register").post(
     ]) // fields accept array 
     ,registerUser) // when this route call then post method call pass to controller
 router.route("/login").post(loginUser)
+
+
 router.route("/logout").post(verifyJWT,logOutUser) // first verify user is login or not that why using verifyJWT middleware
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT,changeCurrentPasssword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImge)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 export default router
