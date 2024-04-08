5688ecccd9778f29d7a18a80b5c3fba98a5896cb
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2024-04-08 23:18:39 +0530

    write getAllvideos and publishVideo  code logic

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,10 +9,111 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler( async(req,res)=>{
     const {page=1,limit=10,query,sortBy,sortType,userId}=req.query
+
+    if(!userId){
+        throw new ApiError(400,"userid not found")
+    }
+
+    const pipeline=[]
+
+    if(userId){
+        const user=await User.findById(userId)
+
+        pipeline.push({
+            $match:{
+                owner: new mongoose.Types.ObjectId(user?._id)
+            }
+        })
+    }
+
+    if(query){
+        pipeline.push({
+            $match:{
+                isPublished:false
+            }
+        })
+    }
+
+   let createField={}
+
+   if(sortBy && sortType){
+    createField[sortBy]= sortType === "asc" ? 1:-1
+
+    pipeline.push({
+        $sort:createField
+    })
+   }
+   else{
+     createField["createdAt"]=-1
+     pipeline.push({
+        $sort:createField
+     })
+   }
+   pipeline.push({
+    $skip:(page-1)*limit
+   })
+   pipeline.push({
+    $limit:limit
+   })
+
+   const allVideos = await Video.aggregate(pipeline)
+
+   if (!allVideos) {
+     throw new ApiError(400,"pipeline aggregation problem")
+   }
+
+   res.status(200).json(
+    new ApiResponse(200,allVideos,`get all videos count : ${allVideos.length}`)
+   )
 })
 
 const publishAVideo=asyncHandler(async(req,res)=>{
     const {title,description}=req.body
+
+    if(!title?.trim() || !description?.trim()){
+        throw new ApiError(400,"Title and description are required ")
+    }
+
+    const thumbnailLocalpath=req.files?.thumbnail[0].path
+    const videoLocalpath=req.files?.videoFile[0].path
+
+    if (!thumbnailLocalpath) {
+        throw new ApiError(400,"thumbnail file is required")
+    }
+    if (!videoLocalpath) {
+        throw new ApiError(400,"video file is required")
+    }
+
+   const uploadThumbnail= await uploadOnCloudinary(thumbnailLocalpath)
+   const uploadVideoFile = await uploadOnCloudinary(videoLocalpath)
+
+   if (!uploadThumbnail && !uploadVideoFile) {
+        throw new ApiError(500,"video and thumbnail are not upload on cloudinary")
+   }
+
+   const video= await Video.create({
+        title:title,
+        description,
+        videoFile:uploadVideoFile.url,
+        thumbnail:uploadThumbnail.url,
+        duration:uploadVideoFile.duration,
+        owner:new mongoose.Types.ObjectId(req.user?._id)
+   })
+
+   const newVideo= await Video.findById(video._id).select("-owner")
+
+   if(!newVideo){
+    throw new ApiError(500,"somthing went wrong when publishing video")
+   }
+
+   return res 
+              .status(200)
+              .json(
+                new ApiResponse(
+                    200,
+                    newVideo, 
+                    "video published successfully"
+                ))
 })
 
 const getVideoById=asyncHandler(async(req,res)=>{
