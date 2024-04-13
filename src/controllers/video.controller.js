eb83da282965910e8aafe2efb44142861958a360
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2024-04-13 19:50:44 +0530

    create video controller logic

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,7 +4,7 @@ import { Video } from "../models/video.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from '../utils/asyncHandler.js'
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {deleteOnCloudinary, uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler( async(req,res)=>{
@@ -118,18 +118,143 @@ const publishAVideo=asyncHandler(async(req,res)=>{
 
 const getVideoById=asyncHandler(async(req,res)=>{
     const {videoId}=req.params
+
+    if(!videoId.trim()){
+        throw new ApiError(400,"video id is inavalid")
+    }
+
+    const video= await Video.findById(videoId)
+
+    if(!video){
+        throw new ApiError(404,"video not found")
+    }
+
+    return res .status(200)
+               .json(
+                new ApiResponse(200,video,"video found succesfully")
+               )
 })
 
 const updateVideo=asyncHandler(async(req,res)=>{
     const{videoId}=req.params
+    const {title,description}=req.body
+
+    if(!videoId.trim()){
+        throw new ApiError(400, "video id is required")
+    }
+
+    if(!title && ! description){
+        throw new ApiError(400,"title and description are required")
+    }
+
+    const video=await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404,"video not found ")
+    }
+
+    if (video.owner.toString()!= (req.user?._id).toString()) {
+        throw new ApiError(401,"unauthorised user !")
+    }
+
+    const thumbnailLocalpath= req.files?.path
+
+    if (!thumbnailLocalpath) {
+        throw new ApiError(404," new thumbnail file is missing ")
+    }
+
+    const thumbnail= await uploadOnCloudinary(thumbnailLocalpath)
+
+    if (!thumbnail.url) {
+        throw new ApiError(500," facing error while uploding thumbnail")
+    }
+
+    const deleteThumbnail= await deleteOnCloudinary(video.thumbnail?.public_id)
+
+    if (!deleteOnCloudinary) {
+        throw new ApiError (500,"error while delete thumbnail from cloudinary")
+    }
+
+    const newVideo= await Video.findByIdAndUpdate(
+        videoId,{
+            $set:{
+                title,
+                description,
+                thumbnail:thumbnail?.url
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if (!newVideo) {
+        throw new ApiError(500,"eroor while updating video")
+    }
+
+    return res .status(200)
+               .json( 
+                new ApiResponse(200 ,newVideo,"Video Updated successfully")
+               )
 })
 
 const deleteVideo=asyncHandler(async(req,res)=>{
     const {videoId}=req.params
+
+    if(!videoId){
+        throw new ApiError(400,"video id is not valid")
+    }
+
+    const video= await Video.findById(videoId)
+
+    if (!video || !(deleteVideo.owner.toString()=== req.user._id.toString()) ) {
+         throw new ApiError(400,"Cannot find the Video")
+    }
+
+    await deleteOnCloudinary(video.videoFile)
+
+    await Video.findByIdAndDelete(videoId)
+
+
+    return res .status(200)
+               .json(
+                new ApiResponse(
+                    200,
+                    "Video deleted Successfully "
+                )
+               )
 })
 
 const togglePublishstatus=asyncHandler(async(req,res)=>{
     const {videoId}=req.params
+
+    if (!videoId) {
+        throw new ApiError(400,"video id is not valid")
+    }
+
+    const video= await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(400,"Video does not existed")
+    }
+
+    if (video.owner.toString()!= (req.user?._id).toString()) {
+        throw new ApiError(401,"unauthorised user !")
+    }
+
+    video.isPublished=!(Video.isPublished)
+
+    await video.save({validateBeforeSave: false})
+
+    return res.status(200)
+              .json(
+               new ApiResponse(
+                200, 
+                video.isPublished,
+                "check published"
+                )
+            )
+
 })
 
 export {
