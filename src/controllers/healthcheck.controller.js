66a3978edcc3afca56b8693bd6848c7dddbdeeaf
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2024-04-25 14:19:36 +0530

    health chek controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,6 +4,16 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthCheck=asyncHandler(async(req,res)=>{
 
+    return res
+                .status(200)
+                .json(
+                    new ApiResponse(
+                        200,
+                        {},
+                        "server is healthy"
+                    )
+                )
+
 })
 
 
